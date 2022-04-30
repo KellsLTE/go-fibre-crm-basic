e258ac80978b712ef6cb902bbb7ae185560c915b
Favour Max-Oti <maxotif@gmail.com>
2022-04-30 20:05:10 +0100

    fixed gofiber import

--- main.go
+++ main.go
@@ -1,26 +1,45 @@
 package main
 
 import (
-	"github.com/gofiber/fiber"
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/jinzhu/gorm"
 	"github.com/maximof/go-fibre-crm-basic/database"
+	"github.com/maximof/go-fibre-crm-basic/lead"
 )
 
-func setepRoutes(app *fiber.App)  {
-	app.Get(GetLeads)
-	app.Get(GetLead)
-	app.Post(NewLead)
-	app.Delete(DeleteLead)
+func setupRoutes(app *fiber.App) {
+	app.Get("api/v1/leads", lead.GetLeads)
+	app.Get("api/v1/lead/:id", lead.GetLead)
+	app.Post("api/v1/lead", lead.NewLead)
+	app.Delete("api/v1/lead/:id", lead.DeleteLead)
 }
 
-func initDatabase()  {
-	database
+func initDatabase() {
+	var err error
+	database.DBConn, err = gorm.Open("sqlite3", "leads.db")
+	if err != nil {
+		panic("failed to connect to database")
+	}
+	fmt.Println("Connection opened to database")
+	database.DBConn.AutoMigrate(&lead.Lead{})
+	fmt.Println("Database Migrated")
 }
 
-func main()  {
+func main() {
 	app := fiber.New()
 
-	setepRoutes(app)
+	initDatabase()
 
-	app.Listen(3001)
+	setupRoutes(app)
 
+	app.Listen(":3001")
+
+	defer func(DBConn *gorm.DB) {
+		err := DBConn.Close()
+		if err != nil {
+
+		}
+	}(database.DBConn)
 }
